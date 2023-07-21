aea585f3c3c9865bf758df833551730678a7d2e6
Sharvari Avinash Halmare <sharvarihalmare002@gmail.com>
2023-07-21 19:56:46 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+// C++ program to illustrate
+// power function
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x = 6.1, y = 4.8;
+
+	// Storing the answer in result.
+	double result = pow(x, y);
+
+	// printing the result upto 2
+	// decimal place
+	cout<<x;
+
+	return 0;
+}
+
